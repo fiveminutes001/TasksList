e1047854d70e0e058ca6a5bfc1f30ca69327be71
five minutes <fiveminutes001@gmail.com>
2021-10-06 22:23:04 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -131,23 +131,23 @@
                     </div>
                 </div>
                 <!-- Todo Item 1 -->
-                <div class="row px-3 align-items-center todo-item rounded">
-                    <div class="col-12 p-0 d-flex align-items-center">
-                        <p>
-                            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                Link with href
-                            </a>
-                            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                Button with data-target
-                            </button>
-                        </p>
-                        <div class="collapse" id="collapseExample">
-                            <div class="card card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-                            </div>
+
+                <div class="col-12 p-0 d-flex align-items-center">
+                    <p>
+                        <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Link with href
+                        </a>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                            Button with data-target
+                        </button>
+                    </p>
+                    <div class="collapse" id="collapseExample">
+                        <div class="card card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
                         </div>
                     </div>
                 </div>
+
                 <!-- Todo Item 1 -->
                 <div class="row px-3 align-items-center todo-item rounded">
                     <div class="col-12 p-0 d-flex align-items-center">
