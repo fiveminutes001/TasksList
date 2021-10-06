b2b27436b4d18d6001fc0c31906d9426cac95006
five minutes <fiveminutes001@gmail.com>
2021-10-06 22:04:08 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -116,17 +116,17 @@
             <div class="col mx-auto">
                 <!-- Todo Item 1 -->
                 <div class="row px-3 align-items-center todo-item rounded">
-                    <div class="col-auto m-1 p-0 d-flex align-items-center">
+                    <div class="col-auto col-sm-12 col-md-3 col-lg-3 col-xl-3 m-1 p-0 d-flex align-items-center">
                         <h2 class="m-0 p-0">
                             <i class="fa fa-square-o text-primary btn m-0 p-0 d-none" data-toggle="tooltip" data-placement="bottom" title="Mark as complete"></i>
                             <i class="fa fa-check-square-o text-primary btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Mark as todo"></i>
                         </h2>
                     </div>
-                    <div class="col px-1 m-1 d-flex align-items-center">
+                    <div class="col-auto col-sm-12 col-md-3 col-lg-3 col-xl-3 px-1 m-1 d-flex align-items-center">
                         <input type="text" class="form-control form-control-lg border-0 bg-transparent rounded px-3" value="Buy groceries for next week" title="Buy groceries for next week" />
                     </div>
 
-                    <div class="col-auto m-1 p-0 todo-actions">
+                    <div class="col-auto col-sm-12 col-md-3 col-lg-3 col-xl-3 m-1 p-0 todo-actions">
                         <i class="fa fa-info-circle my-2 px-2 text-black-50 btn" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
                         <label class="date-label my-2 text-black-50">28th Jun 2020</label>
                         <i class="fa fa-pencil text-info btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
