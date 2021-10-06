20e01bac0f0c94b569efad6ed6465f8192af2692
five minutes <fiveminutes001@gmail.com>
2021-10-06 21:29:46 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -58,14 +58,14 @@
 
         <!-- App title section -->
         <div class="row m-1 p-4">
-            <div class="col-2"></div>
-            <div class="col-8">
+            <div class="col-2 col-sm-2 col-md-3 col-lg-3 col-xl-3"></div>
+            <div class="col-8 col-sm-8 col-md-6 col-lg-6 col-xl-6">
                 <div class="p-1 h1 text-primary text-center mx-auto display-inline-block">
                     <i class="fa fa-check bg-primary text-white rounded p-2"></i>
                     My Todo-s
                 </div>
             </div>
-            <div class="col-2"></div>
+            <div class="col-2 col-sm-2 col-md-3 col-lg-3 col-xl-3"></div>
         </div>
 
         <!-- Create todo section -->
