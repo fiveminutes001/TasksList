3ee8d6c582eb7b3504bdf8ba7e1541dad12451d4
five minutes <fiveminutes001@gmail.com>
2021-10-07 18:14:21 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -147,7 +147,7 @@
             <!-- Todo Item 1 -->
             <div class="col-12 col-sm-6 m-0">
                 <div class="row">
-                    <div class="m-0">
+                    <div class="col-12 m-0">
                         <p>
                             <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                                 Button with data-target
@@ -174,7 +174,7 @@
             <!-- Todo Item 2 -->
             <div class="col-12 col-sm-6 m-0">
                 <div class="row">
-                    <div class="m-0">
+                    <div class="col-12 m-0">
                         <p>
                             <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                                 Button with data-target
