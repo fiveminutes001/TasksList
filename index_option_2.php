60f1a5754a6b03e7fe6a03bce784541f4207cf5c
five minutes <fiveminutes001@gmail.com>
2021-10-07 16:43:08 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -93,12 +93,6 @@
                     </div>
                     <!-- Set due date-->
                     <div class="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-end">
-                        <div class="m-0 px-2 d-flex align-items-center">
-                            <label class="text-secondary my-2 p-0 px-1 view-opt-label due-date-label" required>Due date not set</label>
-                            <i class="fa fa-calendar my-2 px-1 text-primary btn due-date-button" data-toggle="tooltip" data-placement="bottom" title="Set a Due date"></i>
-                            <i class="fa fa-calendar-times-o my-2 px-1 text-danger btn clear-due-date-button" data-toggle="tooltip" data-placement="bottom" title="Clear Due date"></i>
-                        </div>
-
                         <div class="px-0 mx-0 mr-2">
                             <button type="submit" class="btn btn-primary">Add</button>
                         </div>
