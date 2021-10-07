9b9b57aa29fc5fb5b04a9743f4cae501185b5bc4
five minutes <fiveminutes001@gmail.com>
2021-10-07 14:00:46 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -220,6 +220,9 @@
                 var forms = document.getElementsByClassName('needs-validation');
                 // Loop over them and prevent submission
                 var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('keyup', function(event) {
+                        form.checkValidity();
+                    });
                     form.addEventListener('submit', function(event) {
                         if (form.checkValidity() === false) {
                             event.preventDefault();
