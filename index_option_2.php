470d03eb122dd85df3db7c05897c23e6470d2dc3
five minutes <fiveminutes001@gmail.com>
2021-10-07 14:57:26 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -78,7 +78,9 @@
                         <div class="col p-0 m-0">
                             <input class="form-control form-control-lg p-1 add-todo-input bg-transparent rounded" type="text" placeholder="Task name" required>
                         </div>
+                    </div>
 
+                    <div class="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
                         <div class="m-0 px-2 d-flex align-items-center">
                             <label class="text-secondary my-2 p-0 px-1 view-opt-label due-date-label">Due date not set</label>
                             <i class="fa fa-calendar my-2 px-1 text-primary btn due-date-button" data-toggle="tooltip" data-placement="bottom" title="Set a Due date"></i>
