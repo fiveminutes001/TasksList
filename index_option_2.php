a04ec9764815f1d3cb2892d8aabdc2bb8874fa41
five minutes <fiveminutes001@gmail.com>
2021-10-07 17:59:17 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -146,15 +146,20 @@
             <div class="col mx-auto">
                 <!-- Todo Item 1 -->
                 <div>
-                    <p>
-                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                            Button with data-target
-                        </button>
-                    </p>
-                    <div class="collapse" id="collapseExample">
-                        <div class="card card-body mb-3">
-                            <div class="row">
-                                <div class="col-12 col-sm-6 m-0">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 m-0">
+                            <p>
+                                <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                    Button with data-target
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="collapse" id="collapseExample">
+                            <div class="col-12 col-sm-6 m-0">
+                                <div class="card card-body mb-3">
                                     <p>details details details details details details details details details details details details details </p>
                                     <i class="fa fa-info-circle my-2 text-black-50" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
                                     <label class="date-label my-2 text-black-50">28th Jun 2020</label>
@@ -167,15 +172,20 @@
                 </div>
                 <!-- Todo Item 2 -->
                 <div>
-                    <p>
-                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
-                            Button with data-target
-                        </button>
-                    </p>
-                    <div class="collapse" id="collapseExample2">
-                        <div class="card card-body mb-3">
-                            <div class="row">
-                                <div class="col-12 col-sm-6 m-0">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 m-0">
+                            <p>
+                                <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                                    Button with data-target
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="collapse" id="collapseExample2">
+                            <div class="col-12 col-sm-6 m-0">
+                                <div class="card card-body mb-3">
                                     <p>details details details details details details details details details details details details details </p>
                                     <i class="fa fa-info-circle my-2 text-black-50" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
                                     <label class="date-label my-2 text-black-50">28th Jun 2020</label>
