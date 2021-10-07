7f906bec8a04e9cff47f4cd3482a87bb61210945
five minutes <fiveminutes001@gmail.com>
2021-10-07 17:38:45 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -76,7 +76,7 @@
                 <div class="col mx-auto">
                     <!-- Set task name-->
                     <div class="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
-                        <div class="col-12  m-0 mt-2">
+                        <div class="col-12  m-0 mt-2 text-center">
                             <label class="date-label my-2 text-black-50">Add new</label>
                         </div>
                         <div class="col-12 col-sm-9 p-0 m-0 mt-2">
