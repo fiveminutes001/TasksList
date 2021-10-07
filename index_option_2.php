c90ff1cadd6f4a6ba71547c0ea0b4a8c2b75128f
five minutes <fiveminutes001@gmail.com>
2021-10-07 17:37:42 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -72,12 +72,12 @@
         <form>
 
             <div class="row m-1 p-3">
-                <label>Add new</label>
+
                 <div class="col mx-auto">
                     <!-- Set task name-->
                     <div class="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
-
                         <div class="col-12 col-sm-9 p-0 m-0 mt-2">
+                            <label class="date-label my-2 text-black-50">Add new</label>
                             <input class="form-control form-control-lg p-1 add-todo-input bg-transparent text-center rounded" type="text" placeholder="Task name" required>
                         </div>
                         <div class="col-12 col-sm-3 p-0 m-0 mt-2">
