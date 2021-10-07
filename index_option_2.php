5921f5682d7f647aee131a6039a33ebbf337fb4c
five minutes <fiveminutes001@gmail.com>
2021-10-07 17:56:01 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -190,16 +190,17 @@
                 <div>
                     <div class="row">
                         <div class="col-12 col-sm-6 m-0">
-
+                            <p>
+                                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
+                                    Button with data-target
+                                </button>
+                            </p>
                         </div>
                     </div>
                     <div class="collapse" id="collapseExample3">
                         <div class="row">
                             <div class="col-12 col-sm-6 m-0">
                                 <div class="card card-body mb-3">
-                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
-                                        Button with data-target
-                                    </button>
                                     <p>details details details details details details details details details details details details details </p>
                                     <i class="fa fa-info-circle my-2 text-black-50" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
                                     <label class="date-label my-2 text-black-50">28th Jun 2020</label>
