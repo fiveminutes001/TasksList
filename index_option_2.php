4a4e02c366c6a58aa903e1de8fcc0081acc5caed
five minutes <fiveminutes001@gmail.com>
2021-10-08 17:54:01 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -144,94 +144,6 @@
         <!-- Todo list section -->
         <div class="row mx-1 pb-3 w-80" id="task-container">
 
-            <!-- Todo Item 1 -->
-            <div class="col-12 col-sm-6 m-0">
-                <div class="row">
-                    <div class="col-12 m-0 p-1 pb-0">
-                        <p class="m-1">
-                            <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                Button with data-target
-                            </button>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="m-0">
-                        <div class="collapse" id="collapseExample">
-
-                            <div class="card card-body ml-2 mr-2 mt-0 mb-3 pt-2 pb-1">
-                                <p>details details details details details details details details details details details details details </p>
-                                <div class="d-flex flex-row align-items-baseline">
-                                    <i class="fa fa-info-circle my-2 text-black-50 m-2" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
-                                    <label class="date-label my-2 text-black-50 m-2">28th Jun 2020</label>
-                                    <i class="fa fa-pencil text-info m-2 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
-                                    <i class="fa fa-trash-o text-danger m-2 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Todo Item 2 -->
-            <div class="col-12 col-sm-6 m-0">
-                <div class="row">
-                    <div class="col-12 m-0 p-1 pb-0">
-                        <p class="m-1">
-                            <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
-                                Button with data-target
-                            </button>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="m-0">
-                        <div class="collapse" id="collapseExample2">
-
-                            <div class="card card-body ml-2 mr-2 mt-0 mb-3 pt-2 pb-1">
-                                <p>details details details details details details details details details details details details details </p>
-                                <div class="d-flex flex-row align-items-baseline">
-                                    <i class="fa fa-info-circle my-2 text-black-50 m-2" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
-                                    <label class="date-label my-2 text-black-50 m-2">28th Jun 2020</label>
-                                    <i class="fa fa-pencil text-info m-2 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
-                                    <i class="fa fa-trash-o text-danger m-2 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Todo Item 3 -->
-            <div class="col-12 col-sm-6 m-0">
-                <div class="row">
-                    <div class="col-12 m-0 p-1 pb-0">
-                        <p class="m-1">
-                            <button class="btn btn-primary w-100" type="button" data-toggle="collapse" data-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
-                                Button with data-target
-                            </button>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="m-0">
-                        <div class="collapse" id="collapseExample3">
-
-                            <div class="card card-body ml-2 mr-2 mt-0 mb-3 pt-2 pb-1">
-                                <p>details details details details details details details details details details details details details </p>
-                                <div class="d-flex flex-row align-items-baseline">
-                                    <i class="fa fa-info-circle my-2 text-black-50 m-2" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
-                                    <label class="date-label my-2 text-black-50 m-2">28th Jun 2020</label>
-                                    <i class="fa fa-pencil text-info m-2 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
-                                    <i class="fa fa-trash-o text-danger m-2 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 
@@ -328,7 +240,11 @@
             return (documentFragment.appendChild(task));
 
         }
+
         //document.querySelector('#task-container').innerHTML = '';
+        document.querySelector('#task-container').appendChild(createTask());
+        document.querySelector('#task-container').appendChild(createTask());
+        document.querySelector('#task-container').appendChild(createTask());
         document.querySelector('#task-container').appendChild(createTask());
     </script>
 
