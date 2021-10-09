0ea0b1f144b82d7f6390a15c83d235fe310130c2
five minutes <fiveminutes001@gmail.com>
2021-10-09 10:14:04 +0300

    working on design

--- index_option_2.php
+++ index_option_2.php
@@ -212,7 +212,7 @@
         function createTask(params) {
             let task = document.createElement('div');
             task.classList.add('col-12', 'col-sm-6', 'm-0');
-            task.classList.add('task-id', params.taskId);
+            task.classList.add('task-', params.taskId);
 
             let rowDiv = document.createElement('div');
             rowDiv.classList.add('row');
