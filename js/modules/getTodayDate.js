d14e5bf108a82dcbab0eda6c8ead2061a8eba246
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-10 11:51:14 +0300

    utils.js

--- js/modules/getTodayDate.js
+++ js/modules/getTodayDate.js
@@ -1,4 +1,5 @@
 function getTodayDate() {
+	console.log('success!');
 	let today = new Date();
 	let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 	return date;
