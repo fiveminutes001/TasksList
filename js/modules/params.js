4e696cdce8989472e597687e379065bbf5798a88
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-10 13:57:44 +0300

    utils.js

--- js/modules/params.js
+++ js/modules/params.js
@@ -1,25 +1,27 @@
-let paramsArr = [
-	{
-		taskId: 1,
-		taskName: 'Task name',
-		taskDetails: 'Task details',
-		dueDate: '21/08/21',
-		canBeDeleted: false,
-	},
-	{
-		taskId: 2,
-		taskName: 'Task 2 name',
-		taskDetails: 'Task 2 details',
-		dueDate: '22/08/21',
-		canBeDeleted: false,
-	},
-	{
-		taskId: 3,
-		taskName: 'Task 3 name',
-		taskDetails: 'Task 3 details',
-		dueDate: '23/08/21',
-		canBeDeleted: true,
-	},
-];
+function getParams() {
+	return [
+		{
+			taskId: 1,
+			taskName: 'Task name',
+			taskDetails: 'Task details',
+			dueDate: '21/08/21',
+			canBeDeleted: false,
+		},
+		{
+			taskId: 2,
+			taskName: 'Task 2 name',
+			taskDetails: 'Task 2 details',
+			dueDate: '22/08/21',
+			canBeDeleted: false,
+		},
+		{
+			taskId: 3,
+			taskName: 'Task 3 name',
+			taskDetails: 'Task 3 details',
+			dueDate: '23/08/21',
+			canBeDeleted: true,
+		},
+	];
+}
 
 export { params };
