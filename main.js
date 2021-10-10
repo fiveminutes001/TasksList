4610ba29dcd6f59aa1ddb94b3289b32e209b6ffe
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-10 17:16:34 +0300

    datepicker

--- main.js
+++ main.js
@@ -7,10 +7,26 @@ window.onload = function () {
 		problemFree: false,
 	});
 
-	$('.datepicker').datepicker({
+	function formatDate(date) {
+		return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+	}
+	var currentDate = formatDate(new Date());
+	$('#setDate').datepicker({
 		format: 'dd/mm/yyyy',
-		startDate: '0d',
-		endDate: '+365d',
+		autoclose: true,
+		todayHighlight: true,
+		startDate: currentDate,
+		minDate: currentDate,
+		maxViewMode: 2,
+	});
+
+	$('#setDate').on('click', function (event) {
+		$('#setDate')
+			.datepicker('show')
+			.on('changeDate', function (dateChangeEvent) {
+				$('#setDate').datepicker('hide');
+				$('#setDate').text(formatDate(dateChangeEvent.date));
+			});
 	});
 
 	setup.initiateTooltips();
