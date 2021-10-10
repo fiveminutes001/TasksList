a25779cdb64153db3c835cbb8ac00ff6afc1f39e
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-10 09:49:57 +0300

    updating new task form

--- js/formBox.js
+++ js/formBox.js
@@ -5,6 +5,13 @@ $(document).ready(function () {
 		var isReadonly = $form.hasClass('is-readonly');
 		$form.find('input,textarea').prop('disabled', isReadonly);
 	});
+
+	function getTodayDate() {
+		let today = new Date();
+		let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+		return date;
+	}
+
 	let newTaskDueDate = toDo.querySelector('#new-task-due-date');
 	newTaskDueDate.setAttribute('value', getTodayDate());
 	newTaskDueDate.setAttribute('min', getTodayDate());
