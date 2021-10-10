dfe7dd31dabc7828c6f8dfb47fddd29b6ffadd49
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-10 14:04:46 +0300

    params.js

--- main.js
+++ main.js
@@ -11,9 +11,7 @@ window.onload = function () {
 
 	$('[data-toggle="tooltip"]').tooltip();
 
-	console.log(task.getTaskData());
-
 	let newTaskDueDate = document.querySelector('#new-task-due-date');
-	newTaskDueDate.setAttribute('value', newTask.getTodayDate());
-	newTaskDueDate.setAttribute('min', newTask.getTodayDate());
+	newTaskDueDate.setAttribute('value', task.getTodayDate());
+	newTaskDueDate.setAttribute('min', task.getTodayDate());
 };
