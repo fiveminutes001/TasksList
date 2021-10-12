57bf4c996e96ef448a58108f3cee8ef36cd61728
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-12 14:38:27 +0300

    first params obj

--- js/modules/getParamsArr.js
+++ js/modules/getParamsArr.js
@@ -1,34 +1,5 @@
 function getParamsArr() {
-	//return Array
-	return [
-		{
-			taskId: 1,
-			taskName: 'Task name',
-			taskDetails: 'Task details',
-			dueDate: '13/10/21',
-			taskStatus: 'Completed',
-			canBeDeleted: false,
-			taskDeleted: false,
-		},
-		{
-			taskId: 2,
-			taskName: 'Task 2 name',
-			taskDetails: 'Task 2 details',
-			dueDate: '22/08/21',
-			taskStatus: 'Not finished',
-			canBeDeleted: false,
-			taskDeleted: false,
-		},
-		{
-			taskId: 3,
-			taskName: 'Task 3 name',
-			taskDetails: 'Task 3 details',
-			dueDate: '10/10/2021',
-			taskStatus: 'Not finished',
-			canBeDeleted: true,
-			taskDeleted: false,
-		},
-	];
+	return paramsArr.initialParams;
 }
 function getTaskIndexInParamsArr(params) {
 	for (let i = 0; i < paramsArr.length; i++) {
--- main.js
+++ main.js
@@ -7,7 +7,37 @@ window.onload = function () {
 		hasProblems: false,
 		problemFree: false,
 	});
-
+	let paramsArr = {
+		initialParams: [
+			{
+				taskId: 1,
+				taskName: 'Task name',
+				taskDetails: 'Task details',
+				dueDate: '13/10/21',
+				taskStatus: 'Completed',
+				canBeDeleted: false,
+				taskDeleted: false,
+			},
+			{
+				taskId: 2,
+				taskName: 'Task 2 name',
+				taskDetails: 'Task 2 details',
+				dueDate: '22/08/21',
+				taskStatus: 'Not finished',
+				canBeDeleted: false,
+				taskDeleted: false,
+			},
+			{
+				taskId: 3,
+				taskName: 'Task 3 name',
+				taskDetails: 'Task 3 details',
+				dueDate: '10/10/2021',
+				taskStatus: 'Not finished',
+				canBeDeleted: true,
+				taskDeleted: false,
+			},
+		],
+	};
 	setup.initiateTooltips();
 	task.setNewTask();
 	task.sendTasksToContainer(task.getParamsArr());
