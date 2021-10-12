23df0b2c3ada0ae0f1b36397478f3f5856c2cf76
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-12 08:36:39 +0300

    delete button

--- js/modules/initialSetup.js
+++ js/modules/initialSetup.js
@@ -11,7 +11,7 @@ function deleteButtonSetup(deleteButton) {
 	$(deleteButton).on('click', function () {
 		const paramsArr = getParamsArr();
 		const taskParams = null;
-		for (params in paramsArr) {
+		for (const params of paramsArr) {
 			if (params.taskId == deleteButton.getAttribute('task-id')) {
 				taskParams = params;
 				break;
