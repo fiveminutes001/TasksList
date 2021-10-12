480ef9d7649effdd5c244c80c3a19e1c8d4b553f
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-12 14:54:48 +0300

    first params obj

--- js/modules/getParamsArr.js
+++ js/modules/getParamsArr.js
@@ -2,21 +2,14 @@ function getParamsArr() {
 	return Window.paramsArr.initialParams;
 }
 
-function getTaskIndexInParamsArr(params) {
-	for (let i = 0; i < paramsArr.length; i++) {
-		if (params.taskId == paramsArr[i].taskId) {
-			console.log('Task ' + params.taskId + ' found in place ' + i + ' in paramsArr.');
-			return i;
-		}
-	}
-	console.log('Task ' + taskId + ' was not found in paramsArr.');
-}
 function updateParamsArr(newTaskParams) {
 	const paramsArr = getParamsArr();
 	try {
 		for (let params of paramsArr) {
 			if (newTaskParams.taskId == params.taskId) {
 				params = Object.assign(params, newTaskParams);
+				const newParamsArr = Object.assign(paramsArr, params);
+				Window.paramsArr.initialParams = newParamsArr;
 				console.log('Task ' + params.taskId + ' updated:', params);
 				return params;
 			}
