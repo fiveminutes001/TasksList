0a39b76bc8fe93f89d787c58338f2fcc967f3321
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 19:41:22 +0300

    date

--- js/modules/db.js
+++ js/modules/db.js
@@ -46,7 +46,7 @@ function sendTasks() {
 	let opts = {
 		method: 'GET',
 		url: 'db.php',
-		params: JSON.stringify(Window.data),
+		params: { q: JSON.stringify(Window.data), dev: 0 },
 	};
 
 	getAllTasks(opts)
