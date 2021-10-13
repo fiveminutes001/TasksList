2b281f8eb9adc563d7e69e09e4df11c06896de80
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 11:10:12 +0300

    task status

--- main.js
+++ main.js
@@ -16,15 +16,22 @@ window.onload = function () {
 			params: { q: 'a', dev: 0 },
 		};
 
-		db.getAllTasks(opts).then(function (response) {
-			setup.setParams(response);
-			task.sendTasksToContainer(task.getParamsArr());
-		});
+		db.getAllTasks(opts)
+			.then(function (response) {
+				setup.setParams(response);
+				task.sendTasksToContainer(task.getParamsArr());
+			})
+			.then(function () {
+				setup.formSetup();
+				console.log('Tasks loaded.');
+			})
+			.catch(function (err) {
+				console.error('Tasks did not load currectly. ', err.statusText);
+			});
 
 		setup.initiateTooltips();
 		task.setNewTask();
 
-		setup.formSetup();
 		searchResults.sortChange();
 		searchResults.filterChange();
 		searchResults.ascendingSort();
