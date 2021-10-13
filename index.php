1b06ee7e53affbbe2c2bf31ae54e193eea7bdd1e
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 19:51:22 +0300

    date

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ include 'php/connect.php';
 		<link rel="stylesheet" href="css/body.css" />
 		<link rel="stylesheet" href="css/formBox.css" />
     
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootlint/1.1.0/bootlint.min.js"></script>
