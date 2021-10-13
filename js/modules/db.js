3340e06fae3b922d338eeeb4db0a731bd07b55dc
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 23:17:21 +0300

    db stuff

--- js/modules/db.js
+++ js/modules/db.js
@@ -1,6 +1,43 @@
 import * as setup from './initialSetup.js';
 import * as task from './task.js';
 
+function getTotalTasks(opts) {
+	let xhr = new XMLHttpRequest();
+	let params = opts.params;
+
+	if (params && typeof params === 'object') {
+		params = Object.keys(params)
+			.map(function (key) {
+				return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+			})
+			.join('&');
+		opts.url = opts.url + '?' + params;
+	}
+
+	xhr.open(opts.method, opts.url, true);
+
+	xhr.onload = function (opts) {
+		if (this.status >= 200 && this.status < 300) {
+			let response = JSON.parse(xhr.responseText);
+			console.log('Total tasks counted successfuly: ', response);
+			resolve(response);
+		} else {
+			console.log('Total tasks count not successful.');
+			reject({
+				status: this.status,
+				statusText: xhr.statusText,
+			});
+		}
+	};
+	xhr.onerror = function () {
+		reject({
+			status: this.status,
+			statusText: xhr.statusText,
+		});
+	};
+
+	xhr.send();
+}
 function getAllTasks(opts) {
 	return new Promise(function (resolve, reject) {
 		let xhr = new XMLHttpRequest();
@@ -21,10 +58,6 @@ function getAllTasks(opts) {
 			if (this.status >= 200 && this.status < 300) {
 				let response = JSON.parse(xhr.responseText);
 				console.log('db query successful: ', response);
-				console.log('db query successful: ', opts.params);
-				if (params.q == 3) {
-					Window.data.totalLength = response;
-				}
 				resolve(response);
 			} else {
 				console.log('db query not successful.');
@@ -56,15 +89,13 @@ function updateTasks(params) {
 }
 
 function getTotalTasksNumber() {
-	return new Promise(function (resolve, reject) {
-		let opts = {
-			method: 'GET',
-			url: 'db.php',
-			params: { q: 3, data: 0 },
-		};
+	let opts = {
+		method: 'GET',
+		url: 'db.php',
+		params: { q: 3, data: 0 },
+	};
 
-		resolve(getAllTasks(opts));
-	});
+	getTotalTasks(opts);
 }
 
 function getTasks() {
