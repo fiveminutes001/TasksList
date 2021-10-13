e6d61efe9b226a21ac2a760bc87bcf43d00fc002
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-14 00:06:08 +0300

    db stuff

--- php/getData.php
+++ php/getData.php
@@ -95,7 +95,6 @@ $response = '';
 switch ($q) {
 	case 0:
 		$data = json_decode($data, true);
-		//var_dump($data);
 		$new_search = new update_query('taskId', $con, $data);
 		$query_result = $new_search->query_results();
 		break;
