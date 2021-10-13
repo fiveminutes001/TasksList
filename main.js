422686ddc7e9de0e77ccecde404d6a4dbee49c01
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 17:51:19 +0300

    form prevent default

--- main.js
+++ main.js
@@ -5,30 +5,6 @@ import * as db from './js/modules/db.js';
 
 window.onload = function () {
 	(function () {
-		bootlint.showLintReportForCurrentDocument([], {
-			hasProblems: false,
-			problemFree: false,
-		});
-
-		let opts = {
-			method: 'GET',
-			url: 'db.php',
-			params: { q: 'a', dev: 0 },
-		};
-
-		db.getAllTasks(opts)
-			.then(function (response) {
-				setup.setParams(response);
-				task.sendTasksToContainer(task.getParamsArr());
-			})
-			.then(function () {
-				setup.formSetup();
-				console.log('Tasks loaded.');
-			})
-			.catch(function (err) {
-				console.error('Tasks did not load currectly. ', err.statusText);
-			});
-
 		function formSubmit() {
 			return new Promise(function (resolve, reject) {
 				var forms = document.querySelectorAll('.needs-validation');
@@ -50,9 +26,34 @@ window.onload = function () {
 			});
 		}
 
+		bootlint.showLintReportForCurrentDocument([], {
+			hasProblems: false,
+			problemFree: false,
+		});
+
+		let opts = {
+			method: 'GET',
+			url: 'db.php',
+			params: { q: 'a', dev: 0 },
+		};
+
+		db.getAllTasks(opts)
+			.then(function (response) {
+				setup.setParams(response);
+				task.sendTasksToContainer(task.getParamsArr());
+			})
+			.then(function () {
+				setup.formSetup();
+				console.log('Tasks loaded.');
+				formSubmit().then(setup.addNewTaskButtonSetup());
+			})
+			.catch(function (err) {
+				console.error('Tasks did not load currectly. ', err.statusText);
+			});
+
 		setup.initiateTooltips();
 		task.setNewTask();
-		formSubmit().then(setup.addNewTaskButtonSetup());
+
 		searchResults.sortChange();
 		searchResults.filterChange();
 		searchResults.ascendingSort();
