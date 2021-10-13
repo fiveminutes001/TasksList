56c76342f25cb62f34365a48e25587194e0ee2a8
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 08:49:27 +0300

    index.php

--- index.php
+++ index.php
@@ -173,7 +173,7 @@ include 'php/connect.php';
 				</div>
 			</template>
 		</div>
-        <script>if(<?= $con ?>) {console.log(<?= $con ?>)};</script>
+        <script>console.log(<?= json_encode($con) ?>);</script>
 		<script type="module" src="main.js"></script>
 	</body>
 </html>
