e042bd6c1426c507c0f6eca6aa2242825149b2cb
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 20:07:01 +0300

    date

--- db.php
+++ db.php
@@ -8,10 +8,10 @@ ini_set('display_errors', 'On');
 include 'php/connect.php';
 
 //get the search input from URL
-$q = $_GET['q'];
-$dev = $_GET['dev'];
-$a = $_POST['a'];
-$b = $_POST['b'];
+$q = $_GET['q'] ? $_GET['q'] : null;
+$dev = $_GET['dev'] ? $_GET['dev'] : null;
+$a = $_POST['a'] ? $_POST['a'] : null;
+$b = $_POST['b'] ? $_POST['b'] : null;
 //getting data from DB
 include 'php/getData.php';
 
