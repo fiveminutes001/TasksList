e6cfc1662be87022c2c88fa9148b4b1625593c33
Elad Sheskin <fiveminutes001@gmail.com>
2021-10-13 23:55:05 +0300

    db stuff

--- php/getData.php
+++ php/getData.php
@@ -50,21 +50,21 @@ class update_query extends query
 		$this->con = $con;
 		foreach ($data as $d) {
 			$this->sql =
-				'INSERT INTO tasks (`taskId`, `taskDetails`, `dueDate`, `taskStatus`, `taskName`, `canBeDeleted`, `taskDeleted`) VALUES (`' .
+				'INSERT INTO tasks (`taskId`, `taskDetails`, `dueDate`, `taskStatus`, `taskName`, `canBeDeleted`, `taskDeleted`) VALUES ('' .
 				$d['taskId'] .
-				'`,`' .
+				'','' .
 				$d['taskDetails'] .
-				'`,`' .
+				'','' .
 				$d['dueDate'] .
-				'`,`' .
+				'','' .
 				$d['taskStatus'] .
-				'`,`' .
+				'','' .
 				$d['taskName'] .
-				'`,`' .
+				'','' .
 				$d['canBeDeleted'] .
-				'`,`' .
+				'','' .
 				$d['taskDeleted'] .
-				'`)';
+				'')';
 			$this->query_results();
 		}
 	}
